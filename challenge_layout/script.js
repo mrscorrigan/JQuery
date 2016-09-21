23f8ec3739bbe226e278139348ed6676926fbfca
Stephen.Corrigan <stephen.corrigan@live.co.uk>
2016-09-21 07:22:29 -0400

    layout tests

---
+++ challenge_layout/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+ 
+
+ 	$("#stream1_btn").on("click", function() {
+ 		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream1").addClass('highlight_stream');
+	});
+	$("#stream2_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream2").addClass('highlight_stream');
+	});
+	$("#stream3_btn").on("click", function() {
+		$(".stream1").removeClass('highlight_stream');
+		$(".stream2").removeClass('highlight_stream');
+		$(".stream3").removeClass('highlight_stream');
+	  	$(".stream3").addClass('highlight_stream');
+	});
+	$("p").click(function(){ 
+	$("p").css("color","red")
+});
+
+	$("h2").hover(function(){ 
+	$("h2").css("background-color","#18DE7B")
+});
+	
+
+	$("a.bottom_button").hover(function(){
+        $(this).css("background-color", "#31A5CC");
+    }, function(){
+    $(this).css("background-color", "rgba(129, 187, 201,.85)");
+
+});
+
+	$("h2").hover(function(){
+		$(this).addClass("header_hover");
+	}, function(){
+		$(this).removeClass("header_hover");
+	});
+
+
+});
